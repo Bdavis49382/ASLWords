498095095f807ee16ddd65bfed26b981066e5484
Ben Davis <bdavis493@byui.edu>
2022-08-09 12:03:38 -0600

    capitalized

--- objectify.py
+++ objectify.py
@@ -6,7 +6,7 @@ with open('words.csv') as source_file:
     reader = csv.reader(source_file)
     for (i,line) in enumerate(reader):
         for word in line:
-            words.append( {'value': word,'page': f'./pages/{i+1}.jpg'})
+            words.append( {'value': word.capitalize(),'page': f'./pages/{i+1}.jpg'})
     print(json.dumps({'words': words}))
     with open('words.json','wt') as write_file:
         write_file.write(json.dumps({'words':words})) 
