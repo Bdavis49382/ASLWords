c9db6112a3472636392a0f6c721023acb00fc6a2
Ben Davis <bdavis493@byui.edu>
2022-08-09 11:51:08 -0600

    added random word button

--- index.js
+++ index.js
@@ -5,13 +5,17 @@ function makeElement(wordObject) {
     word.setAttribute('href',wordObject.page)
     const listItem = document.createElement('li');
     listItem.innerHTML =  `<a href='${wordObject.page}'>${wordObject.value}</a>`
-    console.log(listItem)
     document.querySelector("#wordArea").appendChild(listItem);
 }
 async function getData() {
     const response = await fetch('./words.json');
     const data = await response.json()
     data.words.forEach(makeElement);
+    document.getElementById('randomButton').addEventListener('click',() => giveRandomWord(data.words));
 }
-
+function giveRandomWord(words) {
+    const word = words[Math.floor(Math.random()*words.length)];
+    document.getElementById('randomWord').innerText = word.value;
+    document.getElementById('randomWord').setAttribute('href',word.page)
+}
 getData();
